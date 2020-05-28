0b5c95de54893fd5535561f47a99150f625874b0
Mohammad Qara <mdcr55@gmail.com>
2020-05-28 20:42:19 +0300

    fixing first route

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
@@ -13,6 +13,7 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
@@ -22,11 +23,30 @@ def create_app(test_config=None):
   @TODO: Use the after_request decorator to set Access-Control-Allow
   '''
 
+  # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
     '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+    @app.route('/')
+    def get_categories():
+
+        categories = list(map(Category.format, Category.query.all()))
+        result = {
+            "success": True,
+            "categories": categories
+        }
+        return jsonify(result)
+
     '''
   @TODO: 
   Create an endpoint to handle GET requests for questions, 
